6f48a9ad68705653248df55334f8a167c5453919
Greg Albrecht <gba@onbeep.com>
2016-10-03 14:53:21 -0700

    perhaps working example

--- app.py
+++ app.py
@@ -3,21 +3,41 @@
 import os
 import json
 
+import aprs
 import flask
 
 
 APRSApp = flask.Flask(__name__)
 
 
+# u'location': {u'ts': 1475531304740, u'lon': -94.7016, u'accuracy_m': 10, u'lat': 41.3578}
+CALLSIGN = 'W2GMD-A'
+
 @APRSApp.route('/', methods=['POST'])
 def slash():
-    print 'slash'
-    post_data = json.loads(flask.request.data)
-    print post_data
-    print
+    aprs_conn = aprs.APRS('AUTOMATIC', '15600')
+    aprs_conn.connect()
 
+    post_data = json.loads(flask.request.data)
     if 'location' in post_data:
-        pprint.pprint(post_data['location'])
+        location = post_data['location']
+    else:
+        return 'OK'
+
+    frame = {
+        'destination': 'APRS',
+        'path': 'TCPIP',
+        'source': CALLSIGN,
+        'text': "!%s\\%s7Automatic-to-APRS gateway." %
+            (aprs.util.dec2dm_lat(location['lat']),
+             aprs.util.dec2dm_lng(location['lon']))
+    }
+
+    aprs_frame = aprs.util.format_aprs_frame(frame)
+    print aprs_frame
+    
+    aprs_result = aprs_conn.send(aprs_frame, protocol='TCP')
+    print aprs_result
 
     return 'OK'
 
